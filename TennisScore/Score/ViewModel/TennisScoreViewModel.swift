18b8ed8b2669e3170e19bca13ffac60c0a9746ac
Ananth Bhamidipati <ananthbh@gmail.com>
2021-01-30 17:19:55 +0100

    refactored code to pass the ongoing and win tests

--- TennisScore/Score/ViewModel/TennisScoreViewModel.swift
+++ TennisScore/Score/ViewModel/TennisScoreViewModel.swift
@@ -9,8 +9,99 @@ import Foundation
 
 struct TennisScoreViewModel {
     
-    public func score(for player
-                        : Player) -> (focusPlayer: Player?, status: GameStatus, score: String) {
-        return (.none, .ongoing, "Love all!")
+    private var playerAScore: Int = 0
+    private var playerBScore: Int = 0
+    
+    //MARK: - Public methods
+    
+    public mutating func score(for player: Player) -> (focusPlayer: Player?, status: GameStatus, score: String) {
+        self.addScore(player: player)
+        return (getFocusPlayer(), getStatus(), getScore())
+    }
+    
+    
+    //MARK: - Private methods
+    
+    private mutating func addScore(player: Player) {
+        if player == .playerA {
+            playerAScore += 1
+        } else {
+            playerBScore += 1
+        }
+    }
+    
+    private func getFocusPlayer() -> Player? {
+        if playerAScore == playerBScore {
+            return .none
+        } else if playerAScore > playerBScore {
+            return .playerA
+        } else {
+            return .playerB
+        }
+    }
+    
+    private func getStatus() -> GameStatus {
+        if checkForWin() {
+            return .win
+        }
+        
+        //deuce
+        
+        //advantage
+        return .ongoing
+        
+    }
+    
+    private func getScore() -> String {
+        guard getStatus() == .ongoing else { return " " }
+        if (playerAScore == playerBScore) {
+            return getScoreStringIfEqual()
+        } else {
+            let playerA = calculateScore(score: playerAScore)
+            let playerB = calculateScore(score: playerBScore)
+            return  playerA + " - " + playerB
+        }
+    }
+    
+    //MARK: - Private methods: Game scenarios
+    
+    private func checkForWin() -> Bool {
+        let playerAWinScenario = playerBScore >= 4 && playerBScore >= playerAScore + 2
+        let playerBWinScenario = playerAScore >= 4 && playerAScore >= playerBScore + 2
+        if (playerAWinScenario) || (playerBWinScenario) {
+            return true
+        }
+        return false
+    }
+    
+    
+    //MARK: - Private methods: Helpers
+    
+    private func calculateScore(score: Int) -> String {
+        switch score {
+        case 0:
+            return "Love"
+        case 1:
+            return "15"
+        case 2:
+            return "30"
+        case 3:
+            return "40"
+        default:
+            return "??"
+        }
+    }
+    
+    private func getScoreStringIfEqual() -> String {
+        switch (playerAScore, playerBScore) {
+        case (0,0):
+            return "Love All!"
+        case (1,1):
+            return "15 All!"
+        case (2,2):
+            return "30 All!"
+        default:
+            return "??"
+        }
     }
 }
