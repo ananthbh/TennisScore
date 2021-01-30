e8c6cf6579498f8d55512ae3c3fdeb68df1b19a2
Ananth Bhamidipati <ananthbh@gmail.com>
2021-01-30 16:29:05 +0100

    reordered files into folders. created Player and GameStatus files

---
+++ TennisScore/Score/GameStatus.swift
@@ -0,0 +1,15 @@
+//
+//  GameStatus.swift
+//  TennisScore
+//
+//  Created by Ananth Bhamidipati on 30/01/2021.
+//
+
+import Foundation
+
+enum GameStatus {
+    case normal
+    case deuce
+    case advantage
+    case win
+}
---
+++ TennisScore/Score/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  TennisScore
+//
+//  Created by Ananth Bhamidipati on 30/01/2021.
+//
+
+import Foundation
+
+enum Player {
+    case playerA
+    case playerB
+}
