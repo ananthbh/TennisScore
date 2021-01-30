6ac15d0d33d13be4d4d99c8609a69f55eabf788c
Ananth Bhamidipati <ananthbh@gmail.com>
2021-01-30 15:49:22 +0100

    created new test file for test the TennisScoreViewModel

---
+++ TennisScoreTests/TennisScoreViewModelTests.swift
@@ -0,0 +1,27 @@
+//
+//  TennisScoreViewModelTests.swift
+//  TennisScoreTests
+//
+//  Created by Ananth Bhamidipati on 30/01/2021.
+//
+
+import XCTest
+@testable import TennisScore
+
+class TennisScoreViewModelTests: XCTestCase {
+    
+    var tennisScoreViewModel: TennisScoreViewModel?
+    
+    override func setUp() {
+        tennisScoreViewModel = TennisScoreViewModel()
+    }
+
+    override func tearDown() {
+        tennisScoreViewModel = nil
+    }
+    
+    //MARK: - Tests
+    
+    
+    
+}
